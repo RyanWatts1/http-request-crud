6a8f58bc581e63a0cd6f3fbeb9ca953e0815602e
RyanWatts1 <devshop202@yahoo.com>
2022-06-08 13:06:14 -0400

    added state and params to post

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,13 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div> Hello World
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PostList />} />
+        <Route path="/:id" element={<Post />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
